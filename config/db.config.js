5582f7c10f44a2aed3ab5c1f28f904fca8f09fb9
AmanMU <IT20155520@my.sliit.lk>
2021-08-23 15:33:40 +0530

    added Create session and view session api's

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // url: "mongodb+srv://ispirithalei:wacyamda@ispirithaleidb.nbswy.mongodb.net/ispirithaleitestDB?retryWrites=true&w=majority"
-    url: "mongodb://ispirithalei:wacyamda@ispirithaleiDB-shard-00-00-nbswy.mongodb.net:27017,ispirithaleiDB-shard-00-01-nbswy.mongodb.net:27017,ispirithaleiDB-shard-00-02-nbswy.mongodb.net:27017/ispirithaleitestDB?ssl=true&replicaSet=ispirithaleiDB-shard-0&authSource=admin&retryWrites=true"
+    url: "mongodb+srv://ispirithalei:wacyamda@ispirithaleidb.nbswy.mongodb.net/ispirithaleitestdb?retryWrites=true&w=majority"
 }
