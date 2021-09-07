51e18394850f68b7773e980b21ccbfe6bd95d7d7
di-nethra <anjana.dinethra@gmail.com>
2021-09-08 03:02:29 +0530

    connectivity errors fixed

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb+srv://ispirithalei:wacyamda@ispirithaleidb.nbswy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  url: "mongodb+srv://ispirithalei:wacyamda@ispirithaleidb.nbswy.mongodb.net/ispirithaleitestDB ?retryWrites=true&w=majority",
 };
