665a5b1adca1a564952c77fb1a9613931d1ab82e
Yomal Bandara <yomalb07@gmail.com>
2021-09-12 17:13:43 +0530

    Create a new object to create and save inventory

--- controllers/inventory.controller.js
+++ controllers/inventory.controller.js
@@ -1,9 +1,33 @@
 const db = require("../models");
-const Tutorial = db.tutorials;
+const Inventory = db.inventory;
 
-// Create and Save a new Tutorial
+// Create and Save a new Inventory item
 exports.create = (req, res) => {
-  
+    console.log(req.body);
+
+    const inventory = new Inventory({
+        item_id:req.body.item_id,
+          item_name:req.body.item_name ,
+          supplier_name: req.body.supplier_name,
+          supplier_email: req.body.supplier_email,
+          supplier_contact: req.body.supplier_contact,
+          purchase_price: req.body.purchase_price,
+          registered_date: req.body.registered_date,
+          type_medicine: req.body.type_medicine
+    });
+
+    //Save Inventory Item in the database
+    inventory
+        .save(inventory)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message:
+                err.message || "Some error occurred while creating the Inventory Item."
+            });
+        });
 };
 
 // Retrieve all Tutorials from the database.
