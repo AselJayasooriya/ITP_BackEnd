104ce8fbb11a494cb0545ec4903edee63dbad04a
Wishwa Rathnaweera <86372219+Wishwa98@users.noreply.github.com>
2021-09-19 00:06:26 +0530

    Update Crud added

--- controllers/postInquiry.controller.js
+++ controllers/postInquiry.controller.js
@@ -40,33 +40,54 @@ exports.findAll = (req, res) => {
         alert(err);
       });
   };
+  
+//Find a single inquiry with an id:
+  exports.findOne = (req, res) => {
+    const id = req.params.id;
+  
+    Inquiry.findById(id)
+      .then(data => {
+        if (!data)
+          res.status(404).send({ message: "Not found Inquiry with id " + id });
+        else res.send(data);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .send({ message: "Error retrieving Inquiry with id=" + id });
+      });
+  };
+
+
+
+
 
 // Update a channelling by the id in the request
-exports.put = (req, res) => {
-    if(!req.body){
-        return res.status(400).send({
-            message:"Data you want to update cannot be empty!"
-        });
+exports.update = (req, res) => {
+    if (!req.body) {
+      return res.status(400).send({
+        message: "Data to update can not be empty!"
+      });
     }
-
-    const id= req.params.id;
-    
-    Inquiry.findByIdAndUpdate(id,req.body,{useFindAndModify:false})
-    .then(data =>{
-        if (!data){
-            res.status(400).send({
-                message:'Cannot update the inqury of id =${id} May be inuqiry was not found!'
-            });
-        }else res.send({ message: 'Inquiry was sucessfully updated'});
-    })
-    .catch(err =>{
+  
+    const id = req.params.id;
+  
+    Inquiry.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+      .then(data => {
+        if (!data) {
+          res.status(404).send({
+            message: `Cannot update Inquory with id=${id}. Maybe Tutorial was not found!`
+          });
+        } else res.send({ message: "Inquiry was updated successfully." });
+      })
+      .catch(err => {
         res.status(500).send({
-            message:'Error updating inquiry with id :'+id
+          message: "Error updating Inquiry with id=" + id
         });
-    })
+      });
+  };
 
-};
-
+  
 // Delete a inquiry with specific id in the request
 exports.delete = (req, res) => {
     const id = req.params.id;
