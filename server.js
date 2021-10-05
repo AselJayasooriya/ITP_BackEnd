824cb2184331afea68f26891407a086e9521fd24
Asel Jayasooriya <80502785+AselJayasooriya@users.noreply.github.com>
2021-10-05 18:59:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: process.env.MONGODB_URL });
+  res.json({message: "Welcome to ispirithalei."});
 });
 
 require("./routes/doctorSession.routes")(app);
